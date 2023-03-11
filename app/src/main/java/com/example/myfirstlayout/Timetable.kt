5942b9107e491012ce3fa611d12e95f02fca58f8
Таран Анжелика Александровна <@ngknn.local>
2023-03-11 18:18:38 +0300

    commit

---
+++ app/src/main/java/com/example/myfirstlayout/Timetable.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Timetable : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timetable)
+    }
+}
