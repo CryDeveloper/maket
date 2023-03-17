345d2421e566fcd8ea39ac54a8b484325961e11f
CryDeveloper <fordefdevat@gmail.com>
2023-03-17 23:29:15 +0300

    got zero maket

--- app/src/main/java/com/example/myfirstlayout/BuyTickets.kt
+++ app/src/main/java/com/example/myfirstlayout/BuyTickets.kt
@@ -1,11 +1,35 @@
 package com.example.myfirstlayout
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class BuyTickets : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy_tickets)
+        init()
+    }
+
+    fun init()
+    {
+        var fullName = findViewById<EditText>(R.id.editTextFullName)
+        var mail = findViewById<EditText>(R.id.editTextMail)
+        var btnPay = findViewById<Button>(R.id.btnPay)
+
+        btnPay.setOnClickListener()
+        {
+            if (fullName.text.isNotEmpty() && mail.text.isNotEmpty())
+            {
+                Toast.makeText(this,"SUCCESSFUL",Toast.LENGTH_LONG).show()
+            }
+            else
+            {
+                Toast.makeText(this,"ENTER THE DATA",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myfirstlayout/MainActivity.kt
+++ app/src/main/java/com/example/myfirstlayout/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.myfirstlayout
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    fun init()
+    {
+        var textView = findViewById<TextView>(R.id.twShowMore)
+        var btnBuyTickets = findViewById<Button>(R.id.button)
+
+        textView.setOnClickListener()
+        {
+            val intent = Intent(this, Timetable::class.java)
+            startActivity(intent)
+        }
+        btnBuyTickets.setOnClickListener()
+        {
+            startActivity(Intent(this, BuyTickets::class.java))
+        }
     }
 }
